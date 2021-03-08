28fae405e58a0bdc6f08c569e424f0fd5745b9ba
vcu-chengx4 <78166727+vcu-chengx4@users.noreply.github.com>
2021-03-08 05:03:16 -0500

    last

--- fun/homework.py
+++ fun/homework.py
@@ -1,6 +1,6 @@
 """Homework file for my students to have fun with some algorithms! """
 incoming_list = [1, 2, 3, 5, 8, 13, 21, 34, 55]
-incoming_dict = {"Internazionale": 59, "AC Milan": 56, "Juventus": 52, "AS Roma": 50, "Atalanta B.C.": 49} 
+incoming_dict = {"Internazionale": "Best team", "AC Milan": "Runnersup", "Juventus": "Bronze", "AS Roma": "4th Clinchers", "Atalanta B.C.": "Sadness"} 
 
 def find_greatest_number(incoming_list):
     return_value = max(incoming_list)
@@ -23,11 +23,11 @@ pass
 
 
 def add_list_numbers(incoming_list):
-   return_value = sum(incoming_list)
-   if return_value is None: 
+    if incoming_list is None:
        return 0
-   else:
-       return return_value
+    else: 
+        return_value = sum(incoming_list)
+        return return_value
 """
 Required parameter, incoming_list, should be a list.
 Add all the values together and return it.
@@ -36,11 +36,11 @@ pass
 
 
 def longest_value_key(incoming_dict):
-    return_value = max(len(incoming_dict) for x in incoming_dict )
-    return return_value
+    longest_key = max(incoming_dict, key=lambda x:len(incoming_dict[x]))
+    return longest_key
 """
 Required parameter, incoming_dict, should be a dict.
 Find the KEY that has a value with the highest length, use the len() function
-solution found at https://stackoverflow.com/questions/10895567/find-longest-string-key-in-dictionary
+solution found at https://stackoverflow.com/questions/19845258/list-as-value-in-dictionary-get-key-of-longest-list
 """
 pass
