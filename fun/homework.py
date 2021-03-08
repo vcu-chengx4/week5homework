aafd0eb049fcc8f7e9853259d1db4213d88ef96d
vcu-chengx4 <78166727+vcu-chengx4@users.noreply.github.com>
2021-03-08 06:40:33 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -1,19 +1,30 @@
 """Homework file for my students to have fun with some algorithms! """
 
+
 def find_greatest_number(incoming_list):
-    """Finds the largest number in the list."""
+    """
+    Required parameter, incoming_list, should be a list.
+    Find the largest number in the list.
+    """
     return_value = max(incoming_list)
     return return_value
 
 
 def find_least_number(incoming_list):
-    """Finds the smallest/least number in the list."""
+    """
+    Required parameter, incoming_list, should be a list.
+    Find the smallest/least number in the list.
+    """
     return_value = min(incoming_list)
     return return_value
 
 
 def add_list_numbers(incoming_list):
-    """Adds all the values together and return it."""
+    """
+    Required parameter, incoming_list, should be a list.
+    Add all the values together and return it.
+    """
+
     if incoming_list is None:
         return 0
     return_value = sum(incoming_list)
@@ -21,8 +32,10 @@ def add_list_numbers(incoming_list):
 
 
 def longest_value_key(incoming_dict):
-    """Find the KEY that has a value with the highest length, use the len() function
-    solution found at https://stackoverflow.com/questions/19845258/list-as-value-in-dictionary-get-key-of-longest-list"""
+    """
+    Required parameter, incoming_dict, should be a dict.
+    Find the KEY that has a value with the highest length, use the len() function
+    """
     if incoming_dict is not None:
         if len(incoming_dict) == 0:
             return None
