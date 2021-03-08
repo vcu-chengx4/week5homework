2e75b3e8810e9a5ac327bc5cb180f9a5b3ecb3e8
vcu-chengx4 <78166727+vcu-chengx4@users.noreply.github.com>
2021-03-08 03:38:45 -0500

    return value

--- fun/homework.py
+++ fun/homework.py
@@ -3,40 +3,41 @@ incoming_list = [1, 2, 3, 5, 8, 13, 21, 34, 55]
 incoming_dict = {"Internazionale": 59, "AC Milan": 56, "Juventus": 52, "AS Roma": 50, "Atalanta B.C.": 49} 
 
 def find_greatest_number(incoming_list):
-    max(incoming_list)
+    return_value = max(incoming_list)
+    return return_value
 """
 Required parameter, incoming_list, should be a list.
 Find the largest number in the list.
 """
-find_greatest_number(incoming_list)
 pass
 
 
 def find_least_number(incoming_list):
-    min(incoming_list)
+    return_value = min(incoming_list)
+    return return_value
 """
 Required parameter, incoming_list, should be a list.
 Find the smallest/least number in the list.
 """
-find_least_number(incoming_list)
 pass
 
 
 def add_list_numbers(incoming_list):
-   sum(incoming_list)
+   return_value = sum(incoming_list)
+   return return_value
 """
 Required parameter, incoming_list, should be a list.
 Add all the values together and return it.
 """
-add_list_numbers(incoming_list)
 pass
 
 
 def longest_value_key(incoming_dict):
     len(incoming_dict)
+    return_value = len(incoming_dict)
+    return return_value
 """
 Required parameter, incoming_dict, should be a dict.
 Find the KEY that has a value with the highest length, use the len() function
 """
-longest_value_key(incoming_dict)
 pass
