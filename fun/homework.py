34d809c7e09d4c4d644783a6e9a0fe5f8ab0eb76
vcu-chengx4 <78166727+vcu-chengx4@users.noreply.github.com>
2021-03-08 02:39:42 -0500

    maybe?

--- fun/homework.py
+++ fun/homework.py
@@ -4,37 +4,41 @@
     incoming_dict = {"Internazionale": 59, "AC Milan": 56, "Juventus": 52, "AS Roma": 50, "Atalanta B.C.": 49} 
 
 def find_greatest_number(incoming_list):
+         print(max(incoming_list))
     """
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
     
-    print(max(incoming_list))
+    
     pass
 
 
 def find_least_number(incoming_list):
+         print(min(incoming_list))
     """
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    print(min(incoming_list))
+    
     pass
 
 
 def add_list_numbers(incoming_list):
+        print(sum(incoming_list))
     """
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-    print(sum(incoming_list))
+    
     pass
 
 
 def longest_value_key(incoming_dict):
+        print(len(incoming_dict))
     """
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    print(len(incoming_dict))
+    
     pass
