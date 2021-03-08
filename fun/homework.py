03401a59450c185e04f64b916a03da660eed131b
vcu-chengx4 <78166727+vcu-chengx4@users.noreply.github.com>
2021-03-08 05:39:23 -0500

    fs

--- fun/homework.py
+++ fun/homework.py
@@ -1,6 +1,7 @@
 """Homework file for my students to have fun with some algorithms! """
 incoming_list = [1, 2, 3, 5, 8, 13, 21, 34, 55]
-incoming_dict = {"Internazionale": "Best team", "AC Milan": "Runnersup", "Juventus": "Bronze", "AS Roma": "4th Clinchers", "Atalanta B.C.": "Sadness"} 
+incoming_dict = {"Internazionale": "Best team", "AC Milan": "Runnersup", "Juventus": "Bronze", 
+"AS Roma": "4th Clinchers", "Atalanta B.C.": "Sadness"}
 
 def find_greatest_number(incoming_list):
     return_value = max(incoming_list)
@@ -9,7 +10,6 @@ def find_greatest_number(incoming_list):
 Required parameter, incoming_list, should be a list.
 Find the largest number in the list.
 """
-pass
 
 
 def find_least_number(incoming_list):
@@ -19,20 +19,18 @@ def find_least_number(incoming_list):
 Required parameter, incoming_list, should be a list.
 Find the smallest/least number in the list.
 """
-pass
 
 
 def add_list_numbers(incoming_list):
     if incoming_list is None:
-       return 0
+        return 0
     else: 
-        return_value = sum(incoming_list)
+    return_value = sum(incoming_list)
         return return_value
 """
 Required parameter, incoming_list, should be a list.
 Add all the values together and return it.
 """
-pass
 
 
 def longest_value_key(incoming_dict):
@@ -50,4 +48,3 @@ Required parameter, incoming_dict, should be a dict.
 Find the KEY that has a value with the highest length, use the len() function
 solution found at https://stackoverflow.com/questions/19845258/list-as-value-in-dictionary-get-key-of-longest-list
 """
-pass
