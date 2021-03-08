91db2c892c0b3ec3373c1e4bd269520f74212f42
vcu-chengx4 <78166727+vcu-chengx4@users.noreply.github.com>
2021-03-08 02:59:26 -0500

    hmmm

--- fun/homework.py
+++ fun/homework.py
@@ -1,44 +1,34 @@
 """Homework file for my students to have fun with some algorithms! """
+incoming_list = [1, 2, 3, 5, 8, 13, 21, 34, 55]
+incoming_dict = {"Internazionale": 59, "AC Milan": 56, "Juventus": 52, "AS Roma": 50, "Atalanta B.C.": 49} 
 
-    incoming_list = [1, 2, 3, 5, 8, 13, 21, 34, 55]
-    incoming_dict = {"Internazionale": 59, "AC Milan": 56, "Juventus": 52, "AS Roma": 50, "Atalanta B.C.": 49} 
-
-def find_greatest_number(incoming_list):
-         print(max(incoming_list))
+def find_greatest_number(incoming_list):print(max(incoming_list))
     """
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    
-    
     pass
 
 
-def find_least_number(incoming_list):
-         print(min(incoming_list))
+def find_least_number(incoming_list):print(min(incoming_list))
     """
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    
     pass
 
 
-def add_list_numbers(incoming_list):
-        print(sum(incoming_list))
+def add_list_numbers(incoming_list):print(sum(incoming_list))
     """
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-    
     pass
 
 
-def longest_value_key(incoming_dict):
-        print(len(incoming_dict))
+def longest_value_key(incoming_dict):print(len(incoming_dict))
     """
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    
     pass
